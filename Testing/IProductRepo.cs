6aa11938c79274cd493d4501b6cc01f2517c3d9c
bryantwhitsett <bryantwhitsett71@gmail.com>
2023-06-28 14:38:23 -0400

    Now we're gettin somewhere

---
+++ Testing/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepo
+    {
+        IEnumerable<Product> GetAllProducts();
+
+
+    }
+}
+
