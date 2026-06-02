2d15fa1ace9406bac524ce57c63b6975eb5d3115
bryantwhitsett <bryantwhitsett71@gmail.com>
2026-06-02 15:50:04 -0400

    4ish days on this and actually makes a smidge more sense hahaha

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing;
+
+public interface IProductRepository
+{
+    public IEnumerable<Product> GetAllProducts(); //stubbed out method
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Testing.Models;
+
+namespace Testing;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly IDbConnection _conn;
+
+    public ProductRepository(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+    
+    /* Now that the repo has been called, Repo queries SQL for data -
+     and returns Product objects and stores them in products in -
+     Index method in Product Controller */
+    public IEnumerable<Product> GetAllProducts()//initializes stubbed out method
+    {
+        return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+    }
+}
